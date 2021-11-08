4c8860ecd2e0bb414897df1084ea302f1a07a4bd
amir.rismani <amir.rismani@samim.net>
2021-11-08 15:44:53 +0330

    Linked list - push

---
+++ linked-list/push.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkeList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+
+        return this
+    }
+} 
+
+
+let myLinkedList = new LinkeList(4);
+myLinkedList.push(11);
