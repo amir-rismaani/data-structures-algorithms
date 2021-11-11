3f5418565ab1990f957b44b3cda6e1ff9991c801
amir.rismaani <amir.rismaani@gmail.com>
2021-11-11 17:26:30 +0330

    Doubly linked list - Push method

---
+++ doubly-linked-list-snippets/push.js
@@ -0,0 +1,35 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList{
+    constructor(value){
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this
+    }
+}
+
+let myDoublyLinkedList = new DoublyLinkedList(7);
+myDoublyLinkedList.push(11);
+myDoublyLinkedList.push(18);
+myDoublyLinkedList.push(5);
