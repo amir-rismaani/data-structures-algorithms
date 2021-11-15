d1b8604264143b6abef46daea8174f2b1cdfa35e
amir.rismaani <amir.rismaani@gmail.com>
2021-11-15 19:17:49 +0330

    remove variable -  because we have arrows going the other way

--- doubly-linked-list-snippets/remove.js
+++ doubly-linked-list-snippets/remove.js
@@ -86,11 +86,9 @@ class DoublyLinkedList{
         if(index < 0 || index >= this.length) return undefined;
 
         const temp = this.get(index);
-        const before = temp.prev;
-        const after = temp.next;
 
-        before.next = after;
-        after.prev = before;
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
 
         temp.next = null;
         temp.prev = null;
